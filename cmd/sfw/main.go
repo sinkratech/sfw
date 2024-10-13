8649520a0bd62ea71e954c4320ebeddba0cfd7c6
samuelsih <samuel.205025@mhs.its.ac.id>
2024-10-13 20:49:02 +0700

    fix: broken build

--- cmd/sfw/main.go
+++ cmd/sfw/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io/fs"
 	"os"
+	"strings"
 
+	"github.com/samuelsih/formattag/align"
 	"github.com/sinkratech/sfw/cmd/sfw/feature"
 	"github.com/urfave/cli/v2"
 )
@@ -45,4 +49,39 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		println(err.Error())
 	}
+}
+
+func handleFormatTag(c *cli.Context) error {
+	rootTarget := "."
+	basefs := os.DirFS(".")
+
+	if c.Args().First() != "" {
+		rootTarget = c.Args().First()
+	}
+
+	err := fs.WalkDir(basefs, rootTarget, func(p string, info fs.DirEntry, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if !strings.HasSuffix(info.Name(), ".go") {
+			return nil
+		}
+
+		align.Init(p)
+
+		b, errWalk := align.Do()
+		if errWalk != nil {
+			return fmt.Errorf("format align failed for %s: %w", p, errWalk)
+		}
+
+		errWalk = os.WriteFile(p, b, 0)
+		if errWalk != nil {
+			return fmt.Errorf("cannot write to file %s: %w", p, errWalk)
+		}
+
+		return nil
+	})
+
+	return err
 }
